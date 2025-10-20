90dce93aefccec352c32140ebbc0a6ebaeaed00f
Bluesom <rganendya@gmail.com>
2025-10-20 09:38:30 +0700

    Update Lines

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello Heaven!");
+        System.out.println("Hellop!");
+        System.out.println("adknfk");
     }
 }
